0e08bf7f786adaab29a250ad00661b00e6aa8609
Jeff Schnitzer <jeff@infohazard.org>
2014-11-19 13:36:47 -0800

    Minor javadoc improvements

--- src/main/java/com/voodoodyne/jackson/jsog/JSOGRefDeserializer.java
+++ src/main/java/com/voodoodyne/jackson/jsog/JSOGRefDeserializer.java
@@ -1,16 +1,15 @@
 package com.voodoodyne.jackson.jsog;
 
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import java.io.IOException;
 
 /**
- * Knows how to take either form of a JSOGRef (number or {@ref:number} and convert it back into a JSOGRef.
+ * Knows how to take either form of a JSOGRef (string or {@ref:string} and convert it back into a JSOGRef.
  *
  * @author Jeff Schnitzer <jeff@infohazard.org>
  */
